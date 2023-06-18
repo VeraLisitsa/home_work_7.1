cc2296c08b7b914419251fab9ad0452aaa114029
Vera Lisitsa <nvk1984@mail.ru>
2023-06-18 16:44:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+
+        long startTs = System.currentTimeMillis(); // start time
+        for (String text : texts) {
+            int maxSize = 0;
+            for (int i = 0; i < text.length(); i++) {
+                for (int j = 0; j < text.length(); j++) {
+                    if (i >= j) {
+                        continue;
+                    }
+                    boolean bFound = false;
+                    for (int k = i; k < j; k++) {
+                        if (text.charAt(k) == 'b') {
+                            bFound = true;
+                            break;
+                        }
+                    }
+                    if (!bFound && maxSize < j - i) {
+                        maxSize = j - i;
+                    }
+                }
+            }
+            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+        }
+        long endTs = System.currentTimeMillis(); // end time
+
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+
+
+        List<Thread> threads = new ArrayList<>();
+        long startTsWithTreads = System.currentTimeMillis();
+        int countThreads = 30;
+
+        for (int t = 0; t < countThreads; t++) {
+            Thread thread = new Thread(() -> {
+                String letters = "aab";
+                int length = 30_000;
+                Random random = new Random();
+                StringBuilder textBuilder = new StringBuilder();
+                for (int i = 0; i < length; i++) {
+                    textBuilder.append(letters.charAt(random.nextInt(letters.length())));
+                }
+                String text = textBuilder.toString();
+
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
+                    }
+                }
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            });
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        long endTsWithTreads = System.currentTimeMillis();
+        System.out.println("Time: " + (endTsWithTreads - startTsWithTreads) + "ms");
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
